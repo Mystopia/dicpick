6c10da3053c034bef77e78df0daf346eca6b44ad
Ryan Balfanz <133278+RyanBalfanz@users.noreply.github.com>
2023-07-25 20:12:16 +0000

    Remove i18n messages helper scripts

--- compilemessages.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pushd dicpick
-
-../venv/bin/python ../manage.py compilemessages
-
-popd
--- makemessages.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pushd dicpick
-
-../venv/bin/python ../manage.py makemessages --all
-
-popd
